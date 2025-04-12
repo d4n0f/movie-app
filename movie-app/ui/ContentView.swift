4db28465b736d55886f34d95871be11f74557486
Balint Fonad <fonadbalint04@gmail.com>
2025-04-12 22:52:59 +0200

    Add basic project setup

--- movie-app/ui/ContentView.swift
+++ movie-app/ui/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(Fonts.title)
+            Text("teszt")
+                .font(Fonts.subheading)
         }
         .padding()
     }

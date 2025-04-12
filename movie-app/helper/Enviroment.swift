7aad897e4ce328b4e20cbdc08681764d193c953f
Balint Fonad <fonadbalint04@gmail.com>
2025-04-13 01:47:09 +0200

    Implement network layer and add support to select DEV or PROD scheme in Xcode

---
+++ movie-app/helper/Enviroment.swift
@@ -0,0 +1,18 @@
+//
+//  Enviroments.swift
+//  movie-app
+//
+//  Created by Balint Fonad on 2025. 04. 12..
+//
+
+struct Environment {
+    enum Name {
+        case prod
+        case dev
+    }
+#if ENV_PROD
+    static let name: Name = .prod
+#else
+    static let name: Name = .dev
+#endif
+}

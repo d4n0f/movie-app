34a18a8554f4808273a3bc304820c6afe0d2fc0b
Balint Fonad <fonadbalint04@gmail.com>
2025-04-22 14:17:06 +0200

    Add basic project setup
    -Add fonts and verify them in ContentView.swift preview
    -Add folders for readability and management

--- movie-app/ui/ContentView.swift
+++ movie-app/ui/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(Fonts.title)
+            Text("teszt")
+                .font(Fonts.subheading)
+
         }
         .padding()
     }

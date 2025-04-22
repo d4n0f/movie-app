5cd84f6cc7de08a21a4bf14471afa3be8ac9d295
Balint Fonad <fonadbalint04@gmail.com>
2025-04-22 13:07:59 +0200

    Initial Commit

---
+++ movie-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie-app
+//
+//  Created by Balint Fonad on 2025. 04. 22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie-app/movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_appApp.swift
+//  movie-app
+//
+//  Created by Balint Fonad on 2025. 04. 22..
+//
+
+import SwiftUI
+
+@main
+struct movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
